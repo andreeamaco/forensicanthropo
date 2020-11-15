c0f9c3c9f98ecd69099b4b38eb1d1d899d7aae9b
AndreeaMaco <andreea.macoveiciuc@gmail.com>
2020-11-15 13:50:12 +0100

    Added quiz

--- skull/skull.js
+++ skull/skull.js
@@ -1,9 +1,55 @@
-let output = document.getElementById('output').innerHTML;
-document.getElementById('startBtn').addEventListener('click', startQuiz);
+const output = document.getElementById('output');
+const quizContainer = document.getElementById('quiz-container');
+const startButton = document.getElementById('startBtn');
+const questionContainer = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
-function startQuiz() {
-  console.log('Start!');
+let questionsOrder, currentQuestionIndex; 
+
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz(e) {
+  e.preventDefault();
+
+  questionsOrder = questions;
+  currentQuestionIndex = 0;
+
+  quizContainer.style="display: initial !important";
+  questionContainer.style="display: initial !important";
+
+  setNextQuestion();
 }
+
+function setNextQuestion() {
+  showQuestion(questionsOrder[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerHTML = `
+    <h3> ${question.question} </h3>
+    <figure>
+      <img class="mb-4 mt-4" src="../images/skull-size.PNG" alt="Skull size">
+      <figcaption style="text-align: center">Image via https://naturalhistory.si.edu/</figcaption>
+    </figure>`;
+}
+
+function selectAnswer() {
+
+
+}
+
+const questions = [
+  {
+    question: 'How big is the skull?',
+    answers: [
+      { text: 'Like option 1', },
+      { text: 'Like option 2', },
+      { text: 'Like option 3', },
+      { text: 'Much smaller/bigger', }
+    ]
+  }
+]
 
 
 
